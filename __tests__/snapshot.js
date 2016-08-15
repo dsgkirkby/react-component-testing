a9ce287c2d042f1b71cf2eadeac8231de3e3781e
Dylan <dylan.pickel@gmail.com>
2016-08-14 22:25:56 -0700

    Switch to jest api and add excluded test for changing values

--- __tests__/snapshot.js
+++ __tests__/snapshot.js
@@ -1,13 +1,24 @@
-import renderer from 'react-test-renderer';
-import Login from '../src/login';
-import React from 'react';
+const renderer = require('react-test-renderer');
+// const TestUtils = require('react-addons-test-utils');
+const Login = require('../src/login');
+const React = require('react');
 
-test('Form with no errors', () => {
-  const tree = renderer.create(<Login/>).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe('Snapshot Testing', () => {
+  it('renders correctly when there are no errors', () => {
+    const tree = renderer.create(<Login/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-test('Form with errors', () => {
-  const tree = renderer.create(<Login error={'test error'}/>).toJSON();
-  expect(tree).toMatchSnapshot();
+  xit('updates the input values when changed', () => {
+    const tree = renderer.create(<Login/>);
+
+    // Change the input values - waiting on selector API for test renderer as mentioned in https://github.com/facebook/jest/issues/1411
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders correctly when there are errors', () => {
+    const tree = renderer.create(<Login error={'test error'}/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
