d3e04db44f8992ae6b3c735b2093e0b15d5b3021
Dylan <dylan.pickel@gmail.com>
2016-08-19 13:16:03 -0700

    Add extra demo stuff

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import Login from './login';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-ReactDOM.render(<Login/>, document.getElementById('root'));
+ReactDOM.render(<Login
+  error="Error"
+  onSubmit={(username, password) => alert(`Username: ${username}, Password: ${password}`)}
+/>, document.getElementById('root'));
